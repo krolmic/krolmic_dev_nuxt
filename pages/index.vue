1832ae3ed8352a52a857a47927b600a013eb9185
Michael Krol <michael.krol@proton.me>
2023-04-25 15:21:15 +0200

    feat: add ContactForm component

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <section>
-        <h1>Web and Mobile Apps Full Stack Development</h1>
-        <h2>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</h2>
+    </section>
+    <section id="contact">
+        <ContactForm />
+        Social media links
     </section>
 </template>
